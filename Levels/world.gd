d8a66c4a67d95e17d0c304a126dfad5eaf671eee
Joey Seeman <joeymsee@gmail.com>
2026-01-31 15:52:54 -0800

    Viewport rework and scoring

--- Levels/world.gd
+++ Levels/world.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
-const VIEWPORT_SPEED_MOD = 0.01 			# Scalar for viewport movement function
+const VIEWPORT_SPEED_MOD = .05 			# Scalar for viewport movement function
 const MODULE_PATH = "res://Levels";
 const MODULE_LIST = ["module_0","module_1"] 	# List of module scenes
+const MODULE_LIMIT = 10
 
-var module_count = 0 	# Number of modules loaded
-var module_pos = 0 		# x-offset for the next module
+var module_pos_queue = [0] 		# x-offset for the next module
+var module_queue = []	# queue tracking the number of modules
+var score = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -17,25 +19,37 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	# Move camera proportional to time (in seconds)
-	var speed = max(VIEWPORT_SPEED_MOD*pow(Time.get_ticks_msec(),2)/1000000, 1)
+	var speed = max(VIEWPORT_SPEED_MOD*sqrt(Time.get_ticks_msec()), 2)
 	get_node("Viewport").position += Vector2(speed, 0) # Update viewport speed
-	get_node("Player").SPEED = 300+speed/delta
+	get_node("Player").SPEED = 300+1.1*speed/(delta)
 	
+	# Update score
+	score += floor(100*speed*delta)/100
+	get_node("Viewport/Score/Label").text = "Score: %d"%score
 	
 	#Load scenes
-	if(module_count < 10):
+	if(module_queue.size() < MODULE_LIMIT):
 		var new_module = load("%s/%s.tscn"%[MODULE_PATH, MODULE_LIST.pick_random()]).instantiate()
-		new_module.position += Vector2(module_pos, 0) # Adjust to proper position
+		new_module.position += Vector2(module_pos_queue[0], 0) # Adjust to proper position
 		add_child(new_module)
 		# Update offset
 		var tilemap = new_module.get_node("TileMapLayer")
-		module_pos += tilemap.get_used_rect().size.x*tilemap.tile_set.tile_size.x
-		module_count += 1
-	
-	# TODO: Unload scenes
+		module_pos_queue.push_front(module_pos_queue[0]+tilemap.get_used_rect().size.x*tilemap.tile_set.tile_size.x)
+		
+		# Reduce size of module queue
+		if(module_pos_queue.size() > MODULE_LIMIT/2):
+			module_pos_queue.pop_back()
+		
+		module_queue.push_back(new_module)
+
+	#Unload scenes
+	if(get_node("Player").global_position.x > module_pos_queue[module_pos_queue.size()-1]):
+		module_queue.pop_front().queue_free()
+			
+
 
 # Killbox handling
-func _on_leftbound_body_entered(body: Node2D) -> void:
+func _on_bound_body_entered(body: Node2D) -> void:
 	if body.get_class() == "CharacterBody2D":
 		print("Player is dead!")
 		get_tree().quit()
