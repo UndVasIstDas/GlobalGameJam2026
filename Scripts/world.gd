b2401e4f21c16cc7d6619c393f12486bbc2e98ea
Joey Seeman <joeymsee@gmail.com>
2026-02-01 13:52:16 -0800

    Minor refactor for consistency

--- Scripts/world.gd
+++ Scripts/world.gd
@@ -9,7 +9,7 @@ var module_pos_queue = [0] 		# x-offset for the next module
 var module_queue = []	# queue tracking the number of modules
 var score = 0
 var start_time = 0
-var is_game_over = false
+var is_game_active = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -29,7 +29,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if(is_game_over):
+	if not is_game_active:
 		return 
 	
 	# Move camera proportional to time (in seconds)
@@ -65,10 +65,11 @@ func _process(delta: float) -> void:
 func _on_bound_body_entered(body: Node2D) -> void:
 	if body.get_class() == "CharacterBody2D":
 		get_node("Viewport/RestartScreen/RestartText").text = "You were caught! Score was: %d"%score
-		is_game_over = true
+		is_game_active = false
 		#get_tree().quit()
 		get_node("Viewport/RestartScreen").visible = true
 
 
 func _on_restart_button_pressed() -> void:
 	get_tree().reload_current_scene()
+	
