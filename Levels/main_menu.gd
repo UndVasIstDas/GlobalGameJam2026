95202d06d2cd29c3cdc0c8a53eb213c7682b1552
Joey Seeman <joeymsee@gmail.com>
2026-02-01 15:55:35 -0800

    Splash screen pending instructions

--- Levels/main_menu.gd
+++ Levels/main_menu.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var started = false
+var num_enter_pressed = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,13 +9,17 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("Start") and not started: load_world(null)
+	if Input.is_action_just_pressed("Start") and num_enter_pressed <= 1: load_world(null)
 		
 		
 func load_world(world):
-	if(world):
+	num_enter_pressed += 1
+	get_node("MainMenu/SplashArt").visible = false
+	if num_enter_pressed == 1:
+		get_node("MainMenu/Instructions").visible = true
+		return
+	elif(world):
 		remove_child(world)
 		world.queue_free()
-	get_node("MainMenu").visible = false
+	get_node("MainMenu/Instructions").visible = false
 	add_child(load("res://Levels/world.tscn").instantiate())
-	started = true
